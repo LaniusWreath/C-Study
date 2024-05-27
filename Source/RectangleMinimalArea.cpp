ef3ad8458458350825fb42777f8551b5a5c0c0bc
LaniusWreath <shrike3040@naver.com>
2024-05-28 02:16:38 +0900

    추가 : N개 점 포함하는 직사각형 최소 넓이 구하기

--- Source/RectangleMinimalArea.cpp
+++ Source/RectangleMinimalArea.cpp
@@ -4,8 +4,8 @@
 
 //점 n개를 둘러싸는 최소 크기의 사각형 넓이 구하기
 
-template<typename T> 
-T FindMin(std::vector<T>& values){ //매개변수로 불러올 때 참조자로 불러오면 메모리 절약. (복사 -> 전달)
+template<typename T>
+T FindMin(std::vector<T>& values) { //매개변수로 불러올 때 참조자로 불러오면 메모리 절약. (복사 -> 전달)
 	return *std::min_element(values.begin(), values.end());
 }
 
