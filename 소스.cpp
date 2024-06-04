f625ad23388376b43fa94ac74b92905f54392713
LaniusWreath <shrike3040@naver.com>
2024-06-05 04:01:11 +0900

    NozzleBall 구조체 및 노즐 선택 재귀함수

--- 소스.cpp
+++ 소스.cpp
@@ -1,41 +1,99 @@
 #include "iostream"
-#include "array"
-#include "string"
+#include "vector"
 
-std::string ClassifyingTriangles(std::array<int, 3> values)
+using namespace std;
+
+
+enum E_NozzleNames
 {
-	std::array<std::string, 4> triangles = { "Equilateral", "Isosceles", "Scalene", "Error" };
-	std::string result = "";
+	A,
+	B,
+	C,
+	D,
+	E,
+	F,
+	NozzleCount = 6,
+};
 
-	if (values[0] and values[1] and values[2] == 60)
+
+struct S_NozzleBall
+{
+	S_NozzleBall* PreCase;
+	vector<vector<int>> NozzleCases;
+	vector<int> RestBalls;
+	int TimeSpent;
+
+	S_NozzleBall(S_NozzleBall* InitPreCase ,vector<vector<int>> InitNozzleCases, vector<int> InitRestBalls, int InitTimeSpent)
 	{
-		result = triangles[0];
-	}
-	else if (values[0] + values[1] + values[2] == 180)
-	{
-		if (values[0] != values[1] and values[0] != values[2] and values[1] != values[2])
-		{
-			result = triangles[2];
-		}
-		else
-		{
-			result = triangles[1];
-		}
-	}
-	else
-	{
-		result = triangles[3];
+		PreCase = InitPreCase;
+		NozzleCases = InitNozzleCases;
+		RestBalls = InitRestBalls;
+		TimeSpent = InitTimeSpent;
 	}
 
-	return result;
+};
+
+//노즐 선택
+void F_InitializeCombination(vector<vector<int>> &CombinationVectors, vector<int> Balls, vector<int> NozzleSelection, int index, int count)
+{
+	if (count == 4)
+	{
+		vector<int> RestBalls = Balls;
+
+		for (int j= 0; j<4; j++)
+		{
+			RestBalls[NozzleSelection[j]]--;
+
+			if (RestBalls[NozzleSelection[j]] < 0)
+			{
+				return;
+			}
+		}
+
+		CombinationVectors.push_back(NozzleSelection);
+		return;
+	}
+
+	for(int i=index; i<6; i++)
+	{
+		NozzleSelection[count] = i;
+
+		F_InitializeCombination(CombinationVectors, Balls, NozzleSelection, i, count+1);
+	}
 }
+
+////노즐로 공 한 번 빼기
+//vector<int> F_CalculateRestball(vector<vector<int>>& CombinationVectors, vector<int> Balls)
+//{
+//	return 
+//}
+
 
 int main()
 {
-	int a, b, c = 0;
-	std::cin >> a >> b >> c;
+	//공 개수 초기화
+	vector<int> Balls(6);
+	for (int i = 0; i < 6; i++)
+	{
+		cin >> Balls[i];
+	}
 
-	std::cout << ClassifyingTriangles({ a,b,c });
+	vector<vector<int>> CombinationVectors;
 
+	S_NozzleBall Head(nullptr, CombinationVectors, Balls, 0);
+
+	vector<int> NozzleSelection(4);
+
+	F_InitializeCombination(CombinationVectors, Balls, NozzleSelection, 0, 0);
+
+	
+	
+	for (const auto& comb : CombinationVectors) {
+		for (int nozzle : comb) {
+			cout << nozzle << " ";
+		}
+		cout << endl;
+	}
+	
 	return 0;
 }
